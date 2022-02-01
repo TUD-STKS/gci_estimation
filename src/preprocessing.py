351b309b969804b2524ca9ab49c65145e20ff8bc
renierts <peter.steiner@tu-dresden.de>
2022-02-01 15:32:23 +0100

    Models can now be optimized, fitted and evaluated.

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -8,6 +8,36 @@ Preprocessing utilities required to reproduce the results in the paper
 import numpy as np
 import librosa
 from scipy.signal import find_peaks
+import csv
+
+
+def peak_picking(y, thr):
+    return librosa.util.peak_pick(y, pre_max=1, post_max=1, pre_avg=1,
+                                  post_avg=1, delta=thr, wait=10)
+
+
+def write_annotation_file(path, intervals, annotations=None):
+    if annotations is not None and len(annotations) != len(intervals):
+        raise ValueError('len(annotations) != len(intervals)')
+
+    with open(path, 'w') as output_file:
+        writer = csv.writer(output_file, delimiter=',')
+
+        if annotations is None:
+            for t_int in intervals:
+                writer.writerow(['%0.3f' % t_int[0], '%0.3f' % t_int[1]])
+        else:
+            for t_int, lab in zip(intervals, annotations):
+                writer.writerow(['%0.3f' % t_int[0], '%0.3f' % t_int[1], lab])
+
+
+def annot_to_time_series(annot, intervals, hop_len):
+    times = np.arange(start=0, stop=intervals[-1, 1], step=0.01)
+    annotations = np.zeros(shape=times.shape)
+    for count, interval in enumerate(intervals):
+        annotations[np.multiply(times >= interval[0], times < interval[1])] =\
+            annot[count]
+    return times, annotations
 
 
 def binarize_signal(y, thr=0.04):
